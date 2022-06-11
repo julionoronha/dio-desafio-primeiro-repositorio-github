57ba961679c9cdecd5121d7477558dda3ba771df
julionoronha <julionoronha96@gmail.com>
2022-06-10 22:02:16 -0300

    Reduzida no codigo

--- TQI Fullstack Developer/Modulo III - Javascript/Sintaxe e operadores/comparaNumeros.js
+++ TQI Fullstack Developer/Modulo III - Javascript/Sintaxe e operadores/comparaNumeros.js
@@ -1,9 +1,35 @@
-var numero1 = 4, numero2 = 40;
+var num1 = 14, num2 = 4;
 
-comparaNumeros(numero1, numero2);
+console.log(criaPrimeiraFrase(num1,num2) + criaSegundaFrase(num1,num2));
+
+function criaPrimeiraFrase (num1, num2)
+{
+    let saoIguais = " ";
+
+    if(num1 != num2)
+    {
+        saoIguais = " não "
+    }
+
+    return "Os números " + num1 +" e " + num2 + saoIguais + "são iguais."
+}
+
+function criaSegundaFrase (num1, num2)
+{
+    let resultado10, resultado20;
+    const soma = num1 + num2;;
+
+    soma > 10 ? resultado10 = "maior " : resultado10 = "menor ";
+    soma > 20 ? resultado20 = "maior " : resultado20 = "menor ";
+
+    return " Sua soma é " + soma + " que é " + resultado10 + "que 10 e " + resultado20 + "que 20.";
+
+}
+/*comparaNumeros(numero1, numero2);
 
 function comparaNumeros(num1, num2)
 {
+    
     let soma = (num1 + num2);
     let saoIguais = (num1 == num2);
     let somaMaiorQue10 = (soma > 10);
@@ -24,3 +50,4 @@ function comparaNumeros(num1, num2)
     process.stdout.write("que 20.");
 
 }
+*/
